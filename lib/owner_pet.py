31a57d7a44ec45dac7c6c493a006b6948e367f75
Bett007 <bettbrian022@gmail.com>
2026-02-01 22:31:20 +0300

    one-to-many lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,19 @@
-class Pet:
-    pass
+# lib/owner.py
+
+from pet import Pet
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner is self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Pet must be an instance of Pet")
+
+        pet.set_owner(self)
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
---
+++ lib/pet.py
@@ -0,0 +1,26 @@
+# lib/pet.py
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type")
+
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = None
+
+        if owner is not None:
+            self.set_owner(owner)
+
+        Pet.all.append(self)
+
+    def set_owner(self, owner):
+        from owner import Owner
+
+        if not isinstance(owner, Owner):
+            raise Exception("Owner must be an instance of Owner")
+
+        self.owner = owner
